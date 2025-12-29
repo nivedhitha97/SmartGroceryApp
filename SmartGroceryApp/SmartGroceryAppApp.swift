1a23f1e281cce7573b0b21ad1693a476b346481a
Nivedhitha Parthasarathy <niveesarati97@gmail.com>
2025-12-29 19:39:04 +0100

    Initial Commit

---
+++ SmartGroceryApp/SmartGroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartGroceryAppApp.swift
+//  SmartGroceryApp
+//
+//  Created by Nivedhitha on 29/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct SmartGroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
